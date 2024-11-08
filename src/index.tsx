8a718a2b5f95a3f26ab9af5ad74d3f7a3f1a8ade
Roman Predein <roman@predein.com>
2024-11-08 15:46:48 +0800

    Implement BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import App from './components/app/app';
+import { BrowserRouter } from 'react-router-dom';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Оборачиваю все приложение в BrowserRouter для включения навигации */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
